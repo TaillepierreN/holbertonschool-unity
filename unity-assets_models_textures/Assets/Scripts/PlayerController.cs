6b688371921bc66c39c496a01255d9096baeeb2c
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-02-03 10:51:41 +0100

    added player controller

---
+++ unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MoveSpeed = 10f;
+    public float JumpForce = 5f;
+
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private float _groundCheckRadius = 0.3f;
+    [SerializeField] private LayerMask _groundLayer;
+    private float _moveX;
+    private float _moveZ;
+    private bool _isGrounded;
+    private bool _jumpPressed;
+
+    void Update()
+    {
+        GetInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundCheckRadius, _groundLayer);
+
+        HandleMovement();
+        HandleJump();
+    }
+
+    private void GetInputs()
+    {
+        _moveX = Input.GetAxis("Horizontal");
+        _moveZ = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            _jumpPressed = true;
+        }
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 moveDirection = new Vector3(_moveX, 0f, _moveZ).normalized;
+        if (moveDirection.magnitude >= 0.1f)
+        {
+            Vector3 move = transform.right * _moveX + transform.forward * _moveZ;
+            Vector3 newPosition = move * MoveSpeed * Time.fixedDeltaTime;
+
+            _rb.MovePosition(_rb.position + newPosition);
+        }
+    }
+
+    private void HandleJump()
+    {
+        if (_jumpPressed)
+        {
+            _rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+            _jumpPressed = false;
+        }
+    }
+    /*private void OnDrawGizmos()
+    {
+        if (_groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(_groundCheck.position, _groundCheckRadius);
+        }
+    }*/
+}
